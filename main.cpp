ba0670f598b332e7b1707afdf83a855e6c0577e5
Clayton Rogers <claytonrogers53@gmail.com>
2022-06-30 19:56:41 -0400

    First working

--- main.cpp
+++ main.cpp
@@ -1,5 +1,67 @@
-//#include <iostream>
+#include <iostream>
+#include <vector>
+#include <random>
 
-int main() {
+class prison_sim_t
+{
+public:
+    prison_sim_t(int num) : num_pris(num), boxes(num), gen(rd()), distrib(0, num)
+    {
+        for (int i = 0; i < num_pris; ++i)
+        {
+            boxes.at(i) = get_rand_box();
+        }
+    }
+
+    bool prisoner_success()
+    {
+        for (int prisoner_num = 0; prisoner_num < num_pris; ++prisoner_num)
+        {
+            int box_no = get_rand_box();
+
+            for (int i = 0; i < (num_pris / 2); ++i)
+            {
+                int value = boxes.at(box_no);
+                if (value == prisoner_num) {
+                    continue;
+                } else {
+                    box_no = value;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+
+    int get_rand_box()
+    {
+        return distrib(gen);
+    }
+
+private:
+    const int num_pris;
+    std::vector<int> boxes;
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_int_distribution<> distrib;
+};
+
+int main()
+{
+
+    const int num_rounds = 100;
+    int num_success = 0;
+    for (int i = 0; i < num_rounds; ++i)
+    {
+        prison_sim_t prison(100);
+        if (prison.prisoner_success())
+        {
+            ++num_success;
+        }
+    }
+
+    std::cout << "Number attempts: " << num_rounds << std::endl;
+    std::cout << "Success ratio: " << (double)num_success / num_rounds << std::endl;
+
     return 0;
 }
